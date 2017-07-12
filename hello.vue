533e9c6f5b0dfd775bfbe0d8ca2daeb16ac22308
KuroHsu <kurotanshi@gmail.com>
2017-07-12 15:43:14 +0800

    first commit

---
+++ hello.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">Hello {{ who }}</div>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      who: 'world'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hello {
+  padding: .5em;
+  font-size: 2em;
+  background-color: #fcf;
+}
+</style>
